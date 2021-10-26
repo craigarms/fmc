a6144c4b782366e09dceaed940adaa47c9c7986a
Craig Armstrong <cag@izc.fr>
2021-10-26 20:30:28 +0200

    Changed API Paths to dict for getAPIPath function

--- libs/fmc_config.py
+++ libs/fmc_config.py
@@ -1,11 +1,11 @@
 FMC_API_BASE = "/api/fmc_config/v1/domain/"
 
-
-URL_NETGRP = "/object/networkgroups?expanded=true"
-URL_HOSTS = "/object/hosts?expanded=true"
-URL_NETS = "/object/networks?expanded=true"
-URL_PORTS = "/object/ports?expanded=true"
-URL_PROTO = "/object/protocolportobjects?expanded=true"
-URL_DEVICES = "/devices/devicerecords?expanded=true"
-URL_DOMAINS = '/api/fmc_platform/v1/info/domain'
-URL_TOKEN = "/api/fmc_platform/v1/auth/generatetoken"
+api_path = {'URL_NETGRP': "/object/networkgroups?expanded=true",
+            'URL_HOSTS': "/object/hosts?expanded=true",
+            'URL_NETS': "/object/networks?expanded=true",
+            'URL_PORTS': "/object/ports?expanded=true",
+            'URL_PROTO': "/object/protocolportobjects?expanded=true",
+            'URL_DEVICES': "/devices/devicerecords?expanded=true",
+            'URL_DOMAINS': '/api/fmc_platform/v1/info/domain',
+            'URL_TOKEN': "/api/fmc_platform/v1/auth/generatetoken"
+            }
